ca04f5529cdeb51eb59f8ca4485038c76cc8ce99
Arpita Bhattacharjee <71565732+Arpi33@users.noreply.github.com>
2021-11-08 04:14:04 +0800

    Update OrganismFactory.java

--- src/OrganismFactory.java
+++ src/OrganismFactory.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 
-//Made by Neo Zhi Ming, all methods made by Neo Zhi Ming
 //PART OF ABSTRACT FACTORY DESIGN PATTERN: An Organism factory that returns the corresponding
 //organism based on the organism type
 public class OrganismFactory extends AbstractFactory{
